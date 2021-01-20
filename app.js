98a034abab0227c2f89764388b96bb1a68a82ee6
Michael Richardson <mor7991@yahoo.com>
2021-01-20 10:41:45 -0600

    finished

--- app.js
+++ app.js
@@ -3,14 +3,41 @@ let me = {
     Career: "Software Dev Student at Innovate Bham",
     Description: "I am a business minded individual with a lot of potential to devote toward a company."
 }
-let myInterests = ["TV","Video Games","Rock Climbing","Streaming on Twitch"];
-let prevExp = ["Paraprofessional at Greystone Elementary School","Full-Time Sales Associate at H&M","Vet Technician at Cat Haven Vet Clinic"];
-let Skills = ["percussion","JavaScript","CSS","HTML","Patience","Team Oriented","Leadership"];
+let myInterests = ["TV", "Video Games", "Rock Climbing", "Streaming on Twitch"];
 
 console.log("Name: " + me.Name.toUpperCase());
 console.log("Career: " + me.Career);
 console.log("Description: " + me.Description);
+console.log(" ");
 
-for(i = 0; i < myInterests.length; i++){
-    console.log(myInterests[i]);
+console.log("My Interests:");
+for (i = 0; i < myInterests.length; i++) {
+    console.log(" * " + myInterests[i]);
+}
+console.log(" ");
+
+console.log("My Previous Experience:");
+displayPosition("Paraprofessional", "Greystone Elementary School", "Helped administer tests and other resources to special needs students");
+displayPosition("Full-Time Sales Associate", "H&M", "Established sales, worked a cash register, learned to multi-task effectively");
+displayPosition("Vet Technician", "Cat Haven Vet Clinic", "Administered vaccines, helped with surgeries, reception");
+console.log(" ");
+
+console.log("My Skills:");
+displaySkill("percussion", true);
+displaySkill("JavaScript", true);
+displaySkill("CSS", false);
+displaySkill("HTML", false);
+displaySkill("Patience", true);
+displaySkill("Team Oriented", true);
+displaySkill("Leadership", true);
+
+function displayPosition(title, name, description) {
+    console.log(" * " + title + " at " + name + " - " + description + ".");
+}
+function displaySkill(skill, cool) {
+    if (cool == true) {
+        console.log(" * BAM: " + skill);
+    } else {
+        console.log(" * " + skill);
+    }
 }
